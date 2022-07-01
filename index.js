d54df2934e53909966f5e4e0dc1853aa536c40d6
mohammad_dn <mdehghani497@gmail.com>
2022-07-01 17:10:44 +0300

    calculator

---
+++ index.js
@@ -0,0 +1,53 @@
+// const leftHand = +prompt('left hand number');
+// const oprator = prompt('oprator');
+// const rightHand = +prompt('right hand number');
+// let result = 0
+// switch (oprator) {
+//     case "*":
+//         result = leftHand * rightHand
+//         break;
+//     case "+":
+//         result = leftHand + rightHand;
+//         break
+//     case "-":
+//         result = leftHand - rightHand
+//         break
+//     case "/":
+//         result = leftHand / rightHand
+//         break
+//     default:
+//         result = 000
+
+
+// }
+// console.log(leftHand)
+// console.log(oprator)
+// console.log(rightHand)
+// console.log(result)
+const leftHand = +prompt('left hand number')
+const oprator = prompt('oprator')
+const rightHand = +prompt('right hand number ')
+let result = 0
+switch (oprator) {
+    case "*":
+        result = leftHand * rightHand
+        break
+    case "+":
+        result = leftHand + rightHand
+        break
+    case "-":
+        result = leftHand - rightHand;
+        break
+    case "/":
+            result = leftHand / rightHand;
+        break
+    default:
+        result = 000
+
+}
+console.log(leftHand)
+console.log(oprator)
+console.log(rightHand)
+console.log(result)
+
+
